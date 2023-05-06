0efba4a37d66b9bf83aa0f6935efae4e75e19518
CHOO\choomyunghoon <9385076@naver.com>
2023-05-07 05:35:33 +0900

    게시판이랑 jdbctemplate 비번수정 헤더에 커뮤니티링크달기

---
+++ skillMarketPrj/src/main/java/com/team4/skillmarket/community/controller/CommunityController.java
@@ -0,0 +1,37 @@
+package com.team4.skillmarket.community.controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.team4.skillmarket.community.service.FreeBoardService;
+import com.team4.skillmarket.community.vo.FreeBoardVo;
+
+@WebServlet("/community/*")
+public class CommunityController extends HttpServlet {
+
+    private FreeBoardService freeBoardService;
+    
+    //커뮤니티 조회하기 ~
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+    	
+    	request.getRequestDispatcher("/WEB-INF/views/community/post/write.jsp").forward(request, response);
+    }
+    
+    //커뮤니티 게시글 작성
+    
+   
+    	
+}
+
+
+
