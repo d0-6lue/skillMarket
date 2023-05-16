79bf8881f10a4df67547628a0fc1f39a1c489d6f
ccoldcat <ccoldcat@gmail.com>
2023-05-17 05:29:20 +0900

    커밋

--- skillMarketPrj/src/main/java/com/team4/skillmarket/admin/notice/controller/AdminNoticeController.java
+++ skillMarketPrj/src/main/java/com/team4/skillmarket/admin/notice/controller/AdminNoticeController.java
@@ -1,12 +1,24 @@
 package com.team4.skillmarket.admin.notice.controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.text.html.HTMLEditorKit.Parser;
+
+import org.json.simple.parser.JSONParser;
+
+import com.google.gson.Gson;
+import com.team4.skillmarket.admin.img.converter.AdminImgConverter;
+import com.team4.skillmarket.admin.notice.vo.AdminNoticeDto;
 
 @WebServlet("/admin/notice")
 public class AdminNoticeController extends HttpServlet{
@@ -14,14 +26,66 @@ public class AdminNoticeController extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		// 어드민 로그인 정보 없으면 로그인 창으로
+		if (req.getSession().getAttribute("AdminLoginVo") == null) {
+			resp.sendRedirect(req.getContextPath() + "/admin/login");
+			return;
+		}
+		
 		req.getRequestDispatcher("/WEB-INF/views/admin/home/notice.jsp").forward(req, resp);
 		
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
+		try {
+		
+		
+			
+			
+		// 요청의 body 값을 읽기 위한 BufferedReader	
+		BufferedReader reader = new BufferedReader(new InputStreamReader(req.getInputStream()));
+		StringBuilder sb = new StringBuilder();	
+		String line;
+		while((line = reader.readLine()) != null) {
+			sb.append(line);
+		}
+		reader.close();
+		
+		// body 값을 문자열로 가져옴
+        String reqBody = sb.toString();
+        
+        // JSON 문자열을 자바 객체로 변환 (Gson 사용)
+        Gson gson = new Gson();
+        AdminNoticeDto dto = gson.fromJson(reqBody, AdminNoticeDto.class);
+        
+        
+        
+        // 자바 객체의 값들을 사용
+        String title = dto.getTitle();
+        String content = dto.getContent();
+        String category = dto.getCategory();
+        String[] images = dto.getImages();
+        
+        System.out.println(content);
+        
+        // 데이터 처리
+        
+        
+        // 응답 설정
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        
+        // 응답 데이터 전송
+        PrintWriter writer = resp.getWriter();
+        writer.write("{\"result\": \"success\"}");
+        writer.close();
+        
+        
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
+
--- skillMarketPrj/src/main/webapp/static/js/admin/notice.js
+++ skillMarketPrj/src/main/webapp/static/js/admin/notice.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+window.onload = function() {
     // 모달창 열기 버튼 클릭 시
     $("#openModalBtn").click(function() {
         $("#myModal").css("display", "block"); // 모달창을 보이게 함
@@ -10,70 +10,114 @@ $(document).ready(function() {
         $("#myModal").css("display", "none"); // 모달창을 숨김
     });
 
-    });
+    
 
     //셀렉트 기본값 삭제
     const catSelect = document.querySelector("#cat_select");
     catSelect.addEventListener("change", () => {
-    if (catSelect.value !== "default") {
-        const defaultOption = catSelect.querySelector('[value="default"]');
-        defaultOption.style.display = "none";
-        }
+	    if (catSelect.value !== "default") {
+	        const defaultOption = catSelect.querySelector('[value="default"]');
+	        defaultOption.style.display = "none";
+	        }
     });
 
+	const submitBtn = document.querySelector("#submitBtn");
+	const titleInput = document.querySelector("#modal_div input");
+	const contentContainer = document.querySelector("#summernote");
+	
+	
+	
 
-    const submitBtn = document.querySelector("#submitBtn");
+	submitBtn.addEventListener("click", () => {
+	    // 제목을 가져온다
+		
+	    const title = titleInput.value;
+	
+	    // 내용을 가져온다
+	    const content = $('#summernote').summernote('code');
+	
+	    // 카테고리를 가져온다
+	    const categorySelect = document.querySelector("#cat_select");
+	    const category = categorySelect.value;
+	
+	    // 이미지들을 가져온다
+	    const images = [];
+	    const imageElements = document.querySelectorAll(".note-editable img");
+	    imageElements.forEach((imageElement) => {
+	        images.push(imageElement.src);
+	    });
+	
+	    // 카테고리가 선택되지 않았으면 alert를 띄운다
+	    if (category === "default") {
+	        alert("카테고리를 선택해주세요");
+	        return;
+	    }
+	
+	    // 제목이 비어있는지 확인한다
+	    if (!title) {
+	        alert("제목을 입력해주세요");
+	        titleInput.style.border = "1px solid red";
+	        return;
+	    }
+	
+	    // 내용이 비어있는지 확인한다
+	    if (!content || content.trim().length === 0) {
+	        alert("내용을 입력해주세요");
+	        contentContainer.style.border = "1px solid red";
+	        return;
+	    }
+	
+	    // 내용의 길이가 5자 이하인 경우 알림을 띄운다
+	    if (content.replace(/(<([^>]+)>)/gi, '').trim().length < 5) {
+	        alert("내용은 5자 이상 입력해주세요");
+	        contentContainer.style.border = "1px solid red";
+	        return;
+	    }
+	
+	    // 서버에 데이터를 보내는 비동기 요청을 보낸다
+	    fetch("/skillmarket/admin/notice", {
+	        method: "POST",
+	        body: JSON.stringify({
+	            title,
+	            content,
+	            category,
+	            images,
+	        }),
+	        headers: {
+	            "Content-Type": "application/json",
+	        },
+	    })
+	        .then((response) => {
+	            if (response.ok) {
+	                alert("등록되었습니다.");
+	                // 등록이 성공하면 모달을 닫는다
+	                const modal = document.querySelector("#myModal");
+	                modal.style.display = "none";
+	            } else {
+	                alert("등록에 실패하였습니다.");
+	            }
+	        })
+	        .catch((error) => {
+	            console.error("Error:", error);
+	        });
+	});
+	
+	// 포커스를 떠날 때 테두리 스타일을 원래 값으로 복구한다
+	titleInput.addEventListener("blur", () => {
+	    if (titleInput.value) {
+	        titleInput.style.border = "";
+	    }
+	});
+	
+	// 포커스를 떠날 때 테두리 스타일을 원래 값으로 복구한다
+	contentContainer.addEventListener("blur", () => {
+	    if (contentContainer.innerHTML.trim().length > 0) {
+	        contentContainer.style.border = "";
+	    }
+	});
 
-    submitBtn.addEventListener("click", async () => {
-    // 제목과 내용을 가져온다
-    const title = document.querySelector("#modal_div input").value;
-    const content = await $('#summernote').summernote('code');
-
-    // 카테고리를 가져온다
-    const categorySelect = document.querySelector("#cat_select");
-    const category = categorySelect.value;
-
-    // 카테고리가 선택되지 않았으면 alert를 띄운다
-    if (category === "default") {
-        alert("카테고리를 선택해주세요");
-        return;
-    }
-    
-    
-
-    // 제목이나 내용이 입력되지 않았으면 alert를 띄운다
-    if (!title || content.trim() === '<p><br></p>' ) {
-    alert("제목과 내용을 입력해주세요");
-    return;
-    }
-
-    if (content.replace(/(<([^>]+)>)/gi, '').trim().length <20 ) {
-    alert("내용을 입력하세요");
-    return;
-    }
-
-   
 
 
-    // 서버에 데이터를 보내는 비동기 요청을 보낸다
-    const response = await fetch("http://example.com/api/notice", {
-    method: "POST",
-    body: JSON.stringify({
-        title,
-        content,
-        category,
-    }),
-    headers: {
-        "Content-Type": "application/json",
-    },
-    });
 
-    if (response.ok) {
-    alert("등록되었습니다.");
-    // 등록이 성공하면 모달을 닫는다
-    const modal = document.querySelector("#myModal");
-    modal.style.display = "none";
-    } else {
-    alert("등록에 실패하였습니다.");
-    }
-});
+
+}
