f1dbd7bfa832e52079122c8fdd381102cee18c12
DO <ehgud5656@naver.com>
2023-05-17 14:27:46 +0900

    요청서 준비

---
+++ skillMarketPrj/src/main/java/com/team4/skillmarket/chat/vo/RequestCategoryVo.java
@@ -0,0 +1,19 @@
+package com.team4.skillmarket.chat.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestCategoryVo {
+	
+	private String categoryNo;
+	private String categoryName;
+
+}
