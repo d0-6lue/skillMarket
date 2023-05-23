2dc823174d956c77791fe0ae2dd423bf6b1346a1
CHOO\choomyunghoon <9385076@naver.com>
2023-05-24 08:18:39 +0900

    템플릿 견적서 트랜잭션처리함..

---
+++ skillMarketPrj/src/main/java/com/team4/skillmarket/estimate/vo/EstimateFaqVo.java
@@ -0,0 +1,17 @@
+package com.team4.skillmarket.estimate.vo;
+
+import lombok.Data;
+
+@Data
+public class EstimateFaqVo {
+	
+	private int estimateFaqNo;
+	private int estimateNo;
+	private String estimateFaqQContent;
+	private String estimateFaqAContent;
+	private String estimateFaqStatus;
+	private String estimateFaqEnrolldate;
+	private String estimateFaqModifyDate;
+
+
+}
